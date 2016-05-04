db8518f3f93f095db2e38c387157680678c86ffc
Sai Man Wong <smwong@kth.se>
2016-05-04 15:31:11 +0200

    Added url/domain verification

--- Domain_Grapher.py
+++ Domain_Grapher.py
@@ -1,32 +1,60 @@
 #!/usr/bin/env python
 from Scrapy_Spider import ScrapySpider
+import config
+import re
+import tldextract
+from urllib2 import urlopen
 
-# Header
-print ('==============================')
-print ('=       Domain Grapher       =')
-print ('= -------------------------- =')
-print ('=                version 0.1 =')
+def valid_url(domain_name):
+    try:
+        if not re.match('^https?://*', domain_name):
+            domain_name = 'http://' + domain_name
+        code = urlopen(domain_name).code
+        # Status codes
+        # 2xx - successful
+        # 3xx - forwarding
+        # >=4xx - error
+        if code / 100 >= 4:
+            return False;
+        else:
+            config.allowed_domains[0] = tldextract.extract(domain_name).registered_domain
+            config.start_urls[0] = domain_name
+            return True;
+    except:
+        return False
 
-# Menu
-print ('==============================')
-print ('=                            =')
-print ('=  1. Enter Domain           =')
-print ('=  2. Start Crawl            =')
-print ('=  3. Drop Tables            =')
-print ('=  0. Exit                   =')
-print ('=                            =')
-print ('==============================')
+while True:
+    # Header
+    print '=============================='
+    print '=       Domain Grapher       ='
+    print '= -------------------------- ='
+    print '=                version 0.1 ='
 
-item = raw_input('Choose between [0-3]: ')
+    # Menu
+    print '=============================='
+    print '=                            ='
+    print '=  1. Change Domain          ='
+    print '=  2. Start Crawl            ='
+    print '=  3. Drop Tables            ='
+    print '=  0. Exit                   ='
+    print '=                            ='
+    print '=============================='
+    print 'Current domain: ' + config.allowed_domains[0]
+    print 'Current url formatted for scrapy: ' + config.start_urls[0]
 
-if item == '1':
-    print('Enter Domain...')
-elif item == '2':
-    print('Starting Crawl...')
-    ss = ScrapySpider()
-elif item == '3':
-    print('Dropping all tables...')
-elif item == '0':
-    print('Exiting...')
-else:
-    print('Invalid input.')
+    item = raw_input('Choose between [0-3]: ')
+
+    if item == '1':
+        domain_name = raw_input('Change domain...\n')
+        if not valid_url(domain_name):
+            print '"' + domain_name + '"' + ' is an invalid domain'
+    elif item == '2':
+        print('Starting Crawl...')
+        ss = ScrapySpider()
+    elif item == '3':
+        print('Dropping all tables...')
+    elif item == '0':
+        print('Exiting...')
+        break
+    else:
+        print('Invalid input.')
